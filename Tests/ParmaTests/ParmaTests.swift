a5fac1c60d0dae1673c8443dd23e54594bedc2d5
Leonard Chan <wxclx98@gmail.com>
2020-08-16 22:42:53 +0800

    Move files back.

--- Tests/ParmaTests/ParmaTests.swift
+++ Tests/ParmaTests/ParmaTests.swift
@@ -6,7 +6,7 @@ final class ParmaTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(Parma().text, "Hello, World!")
+//        XCTAssertEqual(Parma().text, "Hello, World!")
     }
 
     static var allTests = [
